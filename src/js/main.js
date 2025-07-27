bf7d95af6358d3be9327770bdbcf7d2475ce63fd
mrDark315 <ashykhmin04@gmail.com>
2025-07-27 13:03:22 +0300

    Fix Input/label Animations on Focus
    Add js script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
 import '../scss/style.scss'
+
+// TRAVEL REQUEST INPUT ANIMATION
+document.addEventListener('DOMContentLoaded', () => {
+    const inputWrappers = document.querySelectorAll('.input_wrapper');
+
+    inputWrappers.forEach(wrapper => {
+        const input = wrapper.querySelector('input');
+
+        if (!input) return;
+
+        const updateState = () => {
+            if (input.value.length > 0) {
+                wrapper.classList.add('has-value');
+            } else {
+                wrapper.classList.remove('has-value');
+            }
+        };
+
+        input.addEventListener('input', updateState)
+        input.addEventListener('focus', () => {
+            wrapper.classList.add('is-focused');
+        });
+        input.addEventListener('blur', () => {
+            wrapper.classList.remove('is-focused');
+            updateState();
+        });
+
+        updateState();
+    })
+})
