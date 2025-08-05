fba31cb0ab0fd06c4bcb060552622891faa43c4e
mrDark315 <ashykhmin04@gmail.com>
2025-08-05 14:19:57 +0300

    fix air-datepicker (adaptation position)

--- src/js/air-datepicker.js
+++ src/js/air-datepicker.js
@@ -3,9 +3,29 @@ import 'air-datepicker/air-datepicker.css';
 import '../scss/air-datepicker.scss';
 import localeUk from 'air-datepicker/locale/uk';
 
-new AirDatepicker('#date', {
-    locale: localeUk,
-    position: 'bottom center',
-    minDate: new Date(),
-    buttons: ['clear']
-})
+let myDatePicker;
+
+function initializeDatePicker() {
+    // Определяем позицию на основе ширины экрана
+    const screenWidth = window.innerWidth;
+    const position = screenWidth <= 575.98 ? 'bottom left' : 'bottom center';
+
+    // Если экземпляр уже существует, обновляем его
+    if (myDatePicker) {
+        myDatePicker.update({ position: position });
+    } else {
+        // Если нет, создаем новый
+        myDatePicker = new AirDatepicker('#date', {
+            locale: localeUk,
+            position: position,
+            minDate: new Date(),
+            buttons: ['clear']
+        });
+    }
+}
+
+// Инициализируем календарь при первой загрузке
+initializeDatePicker();
+
+// Добавляем обработчик события для изменения размера экрана
+window.addEventListener('resize', initializeDatePicker);
